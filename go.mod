6d9e6015c7697492ba8ca1bc0f601abd21100797
Rai <raimi.rarara@gmail.com>
2021-10-16 09:58:05 +0900

    feat: URLからpathを取得してgo vetを実行

---
+++ go.mod
@@ -0,0 +1,12 @@
+module example.com/mod
+
+go 1.17
+
+require (
+	github.com/gostaticanalysis/skeleton/v2 v2.0.4 // indirect
+	github.com/josharian/txtarfs v0.0.0-20210218200122-0702f000015a // indirect
+	golang.org/x/mod v0.3.0 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html"
+	"net/http"
 	"os/exec"
 )
 
-func main() {
+func goVet(url string) string {
+	// url = "github.com/gostaticanalysis/skeleton/v2"
+	// const url = "github.com/tenntenn/greeting/tree/main/v2"
 
-	const url = "github.com/gostaticanalysis/skeleton/v2"
-	//const url = "github.com/tenntenn/greeting/tree/main/v2"
-
-	list_result, err := exec.Command("go", "list", "-json", "-m", "-versions", url).Output()
+	list_result, err := exec.Command("go", "list", "-json", "-m", "-versions", url).CombinedOutput()
 
 	if err != nil {
 		fmt.Println(err)
@@ -27,19 +28,21 @@ func main() {
 	json.Unmarshal(list_result, &list)
 	fmt.Println(list.Versions)
 
+	var responseData string
+
 	for i := 0; i < len(list.Versions); i++ {
 		urlWithVersion := url + "@" + list.Versions[i]
 
-		_, err := exec.Command("go", "get", urlWithVersion).Output()
+		_, err := exec.Command("go", "get", urlWithVersion).CombinedOutput()
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("get err:", err)
 		}
 
-		list_result_of_version, err := exec.Command("go", "list", "-json", "-m", urlWithVersion).Output()
+		list_result_of_version, err := exec.Command("go", "list", "-json", "-m", urlWithVersion).CombinedOutput()
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("list_result_of_version err: ", err)
 		}
 
 		type Dir struct {
@@ -48,12 +51,33 @@ func main() {
 
 		var path Dir
 		json.Unmarshal(list_result_of_version, &path)
-		fmt.Println(path.Dir)
+		fmt.Println("path_dir: ", path.Dir)
 
-		vet_result, err := exec.Command("go", "vet", "-json", path.Dir+"/...").CombinedOutput()
+	
+		cmd := exec.Command("go", "vet", "-json", "./...")
+
+		cmd.Dir = path.Dir
+        vet_result, err := cmd.CombinedOutput()
+
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("vet_result err: ", err)
 		}
 		fmt.Println(string(vet_result))
+		responseData += urlWithVersion + "\n"
+		responseData += string(vet_result)  + "\n"
 	}
+	return responseData
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s\n", html.EscapeString(r.URL.Path))
+		var response  = goVet(r.URL.Path[1:])
+		fmt.Fprintln(w, response)
+	})
+
+	http.ListenAndServe(":8000", nil)
+
+	
 }
